61c81d5e652719223283c2d4af2085d2369da367
Pagal <ssahillppatell@gmail.com>
2022-10-16 19:12:28 +0530

    Improved Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import { Autocomplete, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 // Creates a simple navbar
 const Navbar = () => {
+  const [stocks, setStocks] = useState(['HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE', 'HUL', 'TCS', 'LT', 'ZOMATO', 'AFFLE']);
+
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div
+      className="flex justify-between items-center p-4"
+    >
         <h1
-          className="text-center text-3xl"
+          className="text-center text-3xl w-10/12"
         >
           Stock View
         </h1>
+        <Autocomplete
+          className='w-2/12'
+          options={stocks}
+          renderInput={(params) => <TextField {...params} label="Stocks" />}
+          onChange={(e, value) => {
+            navigate(`/${value}`);
+          }}
+        />
     </div>
   )
 }
